ea37261bf9adabe572fd3693a3ea66424f5e5666
Ahmed Negm <160288929+x7007x@users.noreply.github.com>
2025-08-05 09:33:46 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='telelite',
-    version='0.1.1',
+    version='0.1.2',
     description='Lightweight Telegram Bot Library',
     author='YourName',
     packages=find_packages(),
