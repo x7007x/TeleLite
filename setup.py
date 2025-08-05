9692e5ac082157185fd7d36f8800bef9fe382ede
Ahmed Negm <160288929+x7007x@users.noreply.github.com>
2025-08-05 06:10:55 +0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='telelite',
+    version='0.1',
+    description='Lightweight Telegram Bot Library',
+    author='YourName',
+    packages=find_packages(),
+    install_requires=[
+        'Flask>=2.2.5',
+        'aiohttp>=3.8.4',
+        'requests>=2.31.0'
+    ],
+    python_requires='>=3.7',
+)
